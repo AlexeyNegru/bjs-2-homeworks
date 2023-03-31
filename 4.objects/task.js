aeae1333d877c6014bf6083f148c11ea52268569
Alex n <Shakal240@gmail.com>
2023-03-31 22:36:05 +0300

    исправление

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,5 +1,4 @@
-function Student(name, gender, age, marks) {
-    function Student(name, gender, age, marks) {
+ function Student(name, gender, age, marks) {
         this.name = name;
         this.gender = gender;
         this.age = age;
@@ -7,7 +6,7 @@ function Student(name, gender, age, marks) {
       }
       new Student("Алиса", "female", 24);
       new Student("Олег", "male", 25); 
-}
+
 
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
@@ -21,16 +20,14 @@ Student.prototype.addMarks = function (...marks) {
 
 Student.prototype.getAverage = function () {
     if (!(this.hasOwnProperty('marks')) || this.marks.length === 0) {
-    return 0;
-    } else {  
-    return this.marks.reduce((acc, item, index, arr) => { 
+         return this.marks.reduce((acc, item, index, arr) => { 
         acc+=item;
         if(index === arr.length - 1) {
              return acc / arr.length;
         } 
         return acc;
         }, 0);
-      }  
+    }  
 }
 
 Student.prototype.exclude = function (reason) {
